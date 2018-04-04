7f946b76d9495bb9edb42444ff7451520077234d
salvinchen <salvinchen@meijiabang.cn>
2018-04-04 09:39:39 +0800

    修改数据库配置，增加跨域设置

---
+++ blogserver/src/main/java/org/sang/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package org.sang.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Administrator on 2017/8/4.
+ */
+@Configuration
+public class CorsConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .maxAge(3600);
+    }
+
+}
